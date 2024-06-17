88200208f1d8d49745d42ec6b2580970dc47fcd9
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-06-17 13:19:33 -0400

    snippet for download data

--- PairTrading/scanner.py
+++ PairTrading/scanner.py
@@ -1,3 +1,7 @@
-print("pairtradeliktheresnotomorrow")
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
 
-patate
+api_key = 'your_api_key'
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='AAPL', interval='1min', outputsize='compact')
+print(data.head())
