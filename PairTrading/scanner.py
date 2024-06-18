37a44bfb1a7b2c7d26106b198302607dccc1843f
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-06-17 21:09:13 -0400

    basic requests exemple

--- PairTrading/scanner.py
+++ PairTrading/scanner.py
@@ -1,7 +1,22 @@
-from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
+import requests
+import configparser
+import io
 
-api_key = 'your_api_key'
-ts = TimeSeries(key=api_key, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='AAPL', interval='1min', outputsize='compact')
-print(data.head())
+config = configparser.ConfigParser()
+config.read('../.config.ini')
+
+interval = 'TIME_SERIES_DAILY'
+symbol = 'NVDA'
+key = config['alpha_vantage']['API_KEY']
+datatype = 'csv'
+
+url = f'https://www.alphavantage.co/query?function={interval}&symbol={symbol}&apikey={key}&datatype={datatype}'
+
+r = requests.get(url).content
+c = pd.read_csv(io.StringIO(r.decode('utf-8')))
+c.to_csv('../data/symbol.csv', index=False)
+
+r = requests.get(f'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={key}').content
+c = pd.read_csv(io.StringIO(r.decode('utf-8')))
+c.to_csv('../data/all_symbol.csv', index=False)
