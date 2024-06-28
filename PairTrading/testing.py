eb9b6845652ef519d3570d54736d93c50efc8c63
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-06-28 10:30:42 -0400

    add ticker details in DB

--- PairTrading/testing.py
+++ PairTrading/testing.py
@@ -3,15 +3,4 @@ from PairTrading.backend.polygon import Polygon
 
 
 p = Polygon()
-
-
-table_name = 'ticker_details'
-df = p.get_table(table_name)
-for t in df.ticker.value_counts():
-	if t > 1:
-		print(t)
-
-column_name = 'ticker'
-value = 'ZXIET'
-# p.cursor.execute(f'DELETE FROM "{table_name}" WHERE "{column_name}" = ?', (value,))
-# p.conn.commit()
+print(p.get_table('ticker_details'))
