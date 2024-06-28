7107c1527f62b5ac9b4ea8466c063692a8397237
Olivier <parties01smacks@icloud.com>
2024-06-28 10:35:04 -0400

    Polygon (#7)
    * cleaning and structuring polygon
    
    * add ticker details in DB

--- PairTrading/testing.py
+++ PairTrading/testing.py
@@ -1,7 +1,6 @@
+import pandas as pd
 from PairTrading.backend.polygon import Polygon
 
 
 p = Polygon()
-df = p.get_table('ticker_details')
-
-print(df[df.ticker == 'MARA'])
+print(p.get_table('ticker_details'))
