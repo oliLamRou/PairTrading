dd6efc26f528d74b9c4f8ecab3c2f33db0900dfe
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-07-02 14:20:02 -0400

    scanner

---
+++ PairTrading/backend/scanner.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from PairTrading.backend.polygon import Polygon
+import math
+
+class Scanner(Polygon):
+    """
+    GOAL: scan and filter tickers for potential pair. NO CHART
+    """
+    def __init__(self):
+        super().__init__()
+        self.min_price = 0
+        self.max_price = 1000000
+        self.min_vol = 0
+        self.max_vol = 1000000000000
+        self.bad_tickers = []
+        self.
+        self._filtered_tickers = []
+
+    @property
+    def filtered_tickers(self):
+        if not self._filtered_tickers:
+            df = self.grouped_daily()
+            df = df[
+                (df.close >= self.min_price) &
+                (df.close <= self.max_price) &
+                (df.close >= self.min_vol) &
+                (df.close >= self.max_vol)
+            ]
+
+        return self._filtered_tickers
+
+
+if __name__ == '__main__':
+    f = Scanner()
+    # f.filter_by_price(min_price=1000000)
+    df = f.grouped_daily()
+    print(df.columns)
