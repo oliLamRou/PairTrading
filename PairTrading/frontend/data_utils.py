16e7c780ae5b1738dc02c964e1371cdc3205338f
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-07-18 13:10:55 -0400

    add avg col function on the fly

--- PairTrading/frontend/data_utils.py
+++ PairTrading/frontend/data_utils.py
@@ -5,6 +5,10 @@ from datetime import date
 
 class DataUtils():
     @staticmethod
+    def get_average(ticker: str, column_name: str, period=30) -> float:
+        return DataWrangler().market_data([ticker])[column_name].rolling(period).mean().to_list()[-1]
+    
+    @staticmethod
     def get_average_volume(ticker: str, period=30) -> float:
         return DataWrangler().market_data([ticker]).volume.rolling(period).mean().to_list()[-1]
     
@@ -24,10 +28,14 @@ class DataUtils():
         return (data - data.min()) / (data.max() - data.min())
         
 if __name__ == '__main__':
+    print('Avg Price:', DataUtils.get_average('AAPL', 'close'))
     print('Avg Vol:', DataUtils.get_average_volume('AAPL'))
     print('Last Close:', DataUtils.get_last_price('AAPL'))
 
     dw = DataWrangler()
     df = dw.market_data(['AAPL'])
-    print('Normalize Max:', DataUtils.normalize_max(df.close))
-    print('Normalize MinMax:', DataUtils.normalize_minmax(df.close))
+    print('Normalize Max:', DataUtils.normalize_max(df.close).tail(5))
+    print('Normalize MinMax:', DataUtils.normalize_minmax(df.close).tail(5))
+
+    #Exemple
+    df['NM'] = DataUtils.normalize_max(df.close)
