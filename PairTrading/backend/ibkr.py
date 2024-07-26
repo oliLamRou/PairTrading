5158e50ac3b75365e9810195dc3d8ccb1bcd863c
mstrgg <gabriel.t.morin@gmail.com>
2024-07-26 16:19:47 -0400

    trades from ibkr

--- PairTrading/backend/ibkr.py
+++ PairTrading/backend/ibkr.py
@@ -1,6 +1,7 @@
 import time, datetime
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+from ibapi.execution import ExecutionFilter
 from ibapi.client import Contract
 import pandas as pd
 import queue
@@ -11,6 +12,7 @@ class IBClient(EWrapper, EClient):
     def __init__(self, host, port, client_id):
         EClient.__init__(self, self)
         self.tick_data_queue = []
+        self.trade_data = []
         self.connect(host, port, client_id)
 
         thread = Thread(target=self.run)
@@ -22,13 +24,31 @@ class IBClient(EWrapper, EClient):
         else:
             print(f'Error {code}: {msg}')
 
+    def execDetails(self, reqId, contract, execution):
+        #print("execDetails", execution)
+        self.trade_data.append({
+            "symbol": contract.symbol,
+            "secType": contract.secType,
+            "exchange": contract.exchange,
+            "side": execution.side,
+            "price": execution.price,
+            "shares": execution.shares,
+            "time": execution.time
+        })
+
+    def execDetailsEnd(self, reqId):
+        print(f"Exec Details End: {reqId}")
+        df = pd.DataFrame(self.trade_data)
+        print(df)
+        self.disconnect()
+
     def historicalData(self, req_id, bar):
         print(bar)
 
     # callback when all historical data has been received
     def historicalDataEnd(self, reqId, start, end):
-        print('hist')
         print(f"end of data {start} {end}")
+        self.disconnect() #a double check
 
     def tickString(self, reqId, tickType, value):
         if tickType == 76:
@@ -62,30 +82,52 @@ class IBClient(EWrapper, EClient):
                 results.append(r)
         return results
     
+class IBUtils():
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def get_trades(ticker: str):
+        client = IBClient('127.0.0.1', 7497, 1)
+        time.sleep(0.2)
+
+        filter = ExecutionFilter()
+        filter.symbol = ticker
+        filter.secType = "STK"
+        client.reqExecutions(2, filter)
+        time.sleep(0.1)
+        client.disconnect()
+        
+        return client.trade_data
+    
 
 if __name__ == '__main__':
+
     print("ib init")
-    client = IBClient('127.0.0.1', 7497, 0)
-    
-    time.sleep(0.3)
+    client = IBClient('127.0.0.1', 7497, 1)
+    api_thread = Thread(target=client.run)
+    api_thread.start()
+    time.sleep(1)
 
-    contract = Contract()
-    contract.symbol = 'AAPL'
-    contract.secType = 'STK'
-    contract.exchange = 'SMART'
-    contract.currency = 'USD'
-    what_to_show = 'TRADES'
+    # contract = Contract()
+    # contract.symbol = 'AAPL'
+    # contract.secType = 'STK'
+    # contract.exchange = 'SMART'
+    # contract.currency = 'USD'
+    # what_to_show = 'TRADES'
 
-    client.reqMktData(3, contract, "47,236", False, False, [])
-    time.sleep(0.8)
-    q = client.get_tick_result(3)
-    print(q)
-    print(client.asynchronous)
-    #print(client.tick_data_queue)
+    # client.reqMktData(3, contract, "47,236", False, False, [])
+    # time.sleep(0.8)
+    # q = client.get_tick_result(3)
+    # print(q)
 
     # client.reqHistoricalData(
     #     2, contract, '', '20 Y', '1 day', what_to_show, True, 2, False, []
     # )
+
+    IBUtils.get_trades("AAPL")
+    time.sleep(0.5)
+    #print(client.trade_data)
     
-    time.sleep(3)
+    time.sleep(5)
     client.disconnect()
--- PairTrading/frontend/test.py
+++ PairTrading/frontend/test.py
@@ -3,13 +3,16 @@ from PairTrading.backend.scanner import Scanner
 from PairTrading.frontend.data_utils import DataUtils as du
 from PairTrading.frontend.ui_pairview import PairView
 from PairTrading.backend.data_wrangler import DataWrangler
+from PairTrading.backend.ibkr import IBUtils as ib
 
 from dash import Dash, html, dcc, Input, Output, State, ctx, ALL
 import dash_bootstrap_components as dbc
 import json
 import pandas as pd
 
+print("test")
+print(ib.get_trades("CMAX"))
 
-data = [{"test1" : 1, "test2" : 2}, {"test1" : 34, "test2" : 9999}]
-df = pd.DataFrame(data=data)
-print(df.to_dict())
+# data = [{"test1" : 1, "test2" : 2}, {"test1" : 34, "test2" : 9999}]
+# df = pd.DataFrame(data=data)
+# print(df.to_dict())
