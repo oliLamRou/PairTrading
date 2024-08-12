2774e1e9c260f6ca03ff70567139f33e7e641ca7
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-08-12 09:21:33 -0400

    pair charts

--- PairTrading/frontend/src/App.vue
+++ PairTrading/frontend/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <!-- <industry/> -->
-  <pair/>
+  <!-- <pair/> -->
+  <chart/>
 </template>
 
 <script setup>
   import industry from '@/components/page/industry.vue';
+  import chart from '@/components/charts/chart_test.vue';
   import pair from '@/components/charts/pair.vue';
 </script>
 
