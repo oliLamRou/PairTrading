9a513065fc03b1f689ce622ca4fd0c46b0c83a2a
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-08-16 17:06:57 -0400

    volume and pair price fix

--- PairTrading/frontend/src/components/form/pair_trade.vue
+++ PairTrading/frontend/src/components/form/pair_trade.vue
@@ -5,7 +5,6 @@
   
   const store = usePairForm();
   const route = useRoute();
-  const data = computed( () => {store.pairs[store.pair].data})
 
   const userInput = reactive({
     reverse: store.pairs[store.pair].reverse,
@@ -17,13 +16,44 @@
   const pair_price = computed(()=> {
     const data = store.pairs[store.pair].data
     if (!data) {
-      return 0
+      return -1
     }
+    let pair_price;
     const last = Math.max.apply(Math, data.map(function(d) { return d.date; }))
     const A_ = data.filter((d) => d.ticker === store.A && d.date === last)[0]
     const B_ = data.filter((d) => d.ticker === store.B && d.date === last)[0]
-    const pair_price = A_.close - (B_.close * userInput.hedge_ratio)
-    return Math.round(pair_price * 100) / 100
+    if (userInput.reverse) {
+      pair_price = B_.close - (A_.close * userInput.hedge_ratio)
+    } else {
+      pair_price = A_.close - (B_.close * userInput.hedge_ratio)
+    }
+    return round_to_two(pair_price)
+  });
+
+  const round_to_two = ((number) => {
+    return Math.round(number * 100) / 100
+  })
+
+  const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+  
+  const avg_vol_A = computed(()=> {
+    const data = store.pairs[store.pair].data
+    if (!data || !data.length > 0) {
+      return -1
+    }  
+    
+    const avg_vol = average( data.filter((d) => d.ticker === store.A).map(item => item.volume) )
+    return Math.round(avg_vol)
+  });  
+
+  const avg_vol_B = computed(()=> {
+    const data = store.pairs[store.pair].data
+    if (!data || !data.length > 0) {
+      return -1
+    }  
+    
+    const avg_vol = average( data.filter((d) => d.ticker === store.B).map(item => item.volume) )
+    return Math.round(avg_vol)
   });
 
   watch(() => userInput.hedge_ratio, (newData) => {
@@ -41,8 +71,8 @@
     <h3>{{store.A}} - {{store.B}}</h3>
     <h5 class="mt-3">Pair Price: ${{pair_price}}</h5>
     <ul>
-      <li>{{store.A}} Dollar volume Average: $999,999,999</li>
-      <li>{{store.B}} Dollar volume Average: $999,999,999</li>
+      <li>{{store.A}} Dollar volume Average: {{avg_vol_A}}</li>
+      <li>{{store.B}} Dollar volume Average: {{avg_vol_B}}</li>
     </ul>
     <hr/>
     <div class="form-check form-switch">
